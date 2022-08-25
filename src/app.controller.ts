141d1f80079cd568da8e85f8da59ec8e1b451eb0
ybh1760 <ybh1760@gmail.com>
2022-08-26 01:48:30 +0900

    feat: setup open api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiResponse({
+    status: 200,
+    description: 'The found record',
+    type: 'string',
+  })
   @Get()
   getHello(): string {
     return this.appService.getHello();
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
+
+function setupSwagger(app: INestApplication): void {
+  const options = new DocumentBuilder()
+    .setTitle('Dormants API Docs')
+    .setDescription('도르멍쓰 api 문서입니다.')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
