e4773ccff876c752ec83038ff0a31ceec7ceeb4a
ybh1760 <ybh1760@gmail.com>
2022-08-25 11:48:48 +0900

    feat: add MysqlProviderModule

---
+++ ormconfig.js
@@ -0,0 +1,14 @@
+module.exports = {
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  charset: 'utf8mb4',
+  entities: ['src/**/*.entity.ts', 'src/**/*.entity.js'],
+  migrations: ['src/database/migrations/*.ts', 'src/database/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/database/migrations',
+  },
+};
