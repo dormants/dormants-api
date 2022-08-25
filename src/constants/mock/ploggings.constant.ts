d662f00fb8a75be4d456a11951d659d03adf0d04
ybh1760 <ybh1760@gmail.com>
2022-08-26 05:57:37 +0900

    feat : add Plogging features

--- src/constants/mock/ploggings.constant.ts
+++ src/constants/mock/ploggings.constant.ts
@@ -1,8 +1,44 @@
-export type Plogging = { id: number; count: number; isFinished: Boolean };
+import { Course } from './courses.constant';
 
-export const MockPloggings: Array<Plogging> = [
-  { id: 0, count: 0, isFinished: false },
-  { id: 1, count: 0, isFinished: false },
-  { id: 2, count: 0, isFinished: false },
-  { id: 3, count: 0, isFinished: false },
-];
+let id = 0;
+
+export class Plogging {
+  id: number;
+  count: number;
+  goal: number;
+  isFinished?: Boolean;
+  course?: Course;
+
+  pickup() {
+    this.count++;
+  }
+
+  checkFinished() {
+    this.isFinished = this.goal === this.count;
+  }
+
+  static from(goal: number, course: Course) {
+    const res = new Plogging();
+    res.goal = goal;
+    res.course = course;
+    res.id = id++;
+    res.count = 0;
+    res.isFinished = false;
+
+    return res;
+  }
+}
+
+class PloggingStorage {
+  ploggings: Plogging[] = [];
+
+  add(plogging: Plogging) {
+    this.ploggings.push(plogging);
+  }
+
+  find(_id: number): Plogging {
+    return this.ploggings.find(({ id }) => _id === id);
+  }
+}
+
+export const ploggingStorage = new PloggingStorage();
--- src/controllers/ploggings.controller.ts
+++ src/controllers/ploggings.controller.ts
@@ -1,26 +1,26 @@
 import { Body, Controller, Post, Param } from '@nestjs/common';
 
-import { MockCourses, MockPloggings, Course } from 'src/constants/mock';
+import { MockCourses, Plogging, ploggingStorage } from 'src/constants/mock';
 
 @Controller('ploggings')
 export class PloggingsController {
   // 혼자 줍기 시작
   @Post()
-  startPlogging(@Body() body: any): any {
-    const course = MockCourses.filter((i) => i.id == Number(body.id));
-    return { count: 0, goal: Number(body.goal), course };
+  startPlogging(
+    @Body() { goal, courseId }: { goal: number; courseId: number },
+  ): any {
+    const course = MockCourses.find((i) => i.id === Number(courseId));
+    const plogging = Plogging.from(goal, course);
+
+    ploggingStorage.add(plogging);
+    return plogging;
   }
 
-  // body로 받은 goal을 달성하면 성공
-  trashCount = 0;
   @Post('/:id/pickup')
-  getTrash(@Param('id') id: string, @Body('goal') goal: number): any {
-    this.trashCount++;
-    const data1 = MockPloggings.filter((i) => i.id == Number(id));
-    const data2 = MockCourses.filter((i) => i.id == Number(id));
-    if (goal == this.trashCount) {
-      return { isFinish: true };
-    }
-    return { count: this.trashCount, ...data1, ...data2 };
+  getTrash(@Param('id') id: string): any {
+    const plogging = ploggingStorage.find(Number(id));
+    plogging.pickup();
+    plogging.checkFinished();
+    return plogging;
   }
 }
