1b23b0d64bf445c6d85ea85fc9b7a1cd4863ddee
ybh1760 <ybh1760@gmail.com>
2022-08-26 06:06:14 +0900

    chore: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   setupSwagger(app);
   await app.listen(3000);
 }
